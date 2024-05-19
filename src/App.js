d745e5ed3d0d006f9d99bec64b319c132a43d0f2
root <you@example.com>
2024-05-19 07:19:25 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -19,70 +19,69 @@ class App extends Component {
 
   //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
 
-
   removeAllCartItem = () => {
-      this.setState({cartList: []})
+    this.setState({cartList: []})
   }
 
   incrementCartItemQuantity = id => {
-      this.setState(prevState => ({
-          cartList: prevState.cartList.map(eachCartItem => {
-              if (id===eachCartItem.id) {
-                  const updatedQuantity = eachCartItem.quantity + 1
-                  return {...eachCartItem, quantity: updatedQuantity}
-              }
-              return eachCartItem
-          }),
-      }))
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachCartItem => {
+        if (id === eachCartItem.id) {
+          const updatedQuantity = eachCartItem.quantity + 1
+          return {...eachCartItem, quantity: updatedQuantity}
+        }
+        return eachCartItem
+      }),
+    }))
   }
 
   decrementCartItemQuantity = id => {
-      const {cartList} = this.state
-      const productObject = cartList.find(eachCartItem => eachCartItem.id === id)
-      if (productObject.quantity > 1) {
-          this.setState(prevState => ({
-             cartList: prevState.cartList.map(eachCartItem => {
-                 if (id === eachCartItem.id) {
-                     const updatedQuantity = eachCartItem.quantity - 1
-                     return {...eachCartItem, quantity: updatedQuantity}
-                 }
-                 return eachCartItem
-             }), 
-          }))
-      } else {
-          this.removeCartItem(id)
-      }
+    const {cartList} = this.state
+    const productObject = cartList.find(eachCartItem => eachCartItem.id === id)
+    if (productObject.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (id === eachCartItem.id) {
+            const updatedQuantity = eachCartItem.quantity - 1
+            return {...eachCartItem, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      this.removeCartItem(id)
+    }
   }
 
-  removeCartItem = id = {
-      const {cartList} = this.state
-      const updatedCartList = cartList.filter(
-          eachCartItem => eachCartItem.id !== id,
-      )
-      this.setState({cartList: updatedCartList})
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const updatedCartList = cartList.filter(
+      eachCartItem => eachCartItem.id !== id,
+    )
+    this.setState({cartList: updatedCartList})
   }
 
   addCartItem = product => {
     const {cartList} = this.state
     const productObject = cartList.find(
-        eachCartItem => eachCartItem.id === product.id,
+      eachCartItem => eachCartItem.id === product.id,
     )
 
     if (productObject) {
-        this.setState(prevState => ({
-            cartList: prevState.cartList.map(eachCartItem => {
-                if (productObject.id === eachCartItem.id) {
-                    const updatedQuantity = eachCartItem.quantity + product.quantity
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (productObject.id === eachCartItem.id) {
+            const updatedQuantity = eachCartItem.quantity + product.quantity
 
-                    return {...eachCartItem, quantity: updatedQuantity}
-                }
-                return eachCartItem
-            }),
-        }))
+            return {...eachCartItem, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
     } else {
-        const updatedCartList = [...cartList, product]
+      const updatedCartList = [...cartList, product]
 
-        this.setState({cartList: updatedCartList})
+      this.setState({cartList: updatedCartList})
     }
   }
 
@@ -102,7 +101,7 @@ class App extends Component {
       >
         <Switch>
           <Route exact path="/login" component={LoginForm} />
-          <ProtectedRoute exact path="/" component={Home} />    
+          <ProtectedRoute exact path="/" component={Home} />
           <ProtectedRoute exact path="/products" component={Products} />
           <ProtectedRoute
             exact
